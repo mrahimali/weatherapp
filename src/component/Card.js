fda7f39cd884a1daf4ba47b14fab9384c8200219
Rahim <mrahimali97@gmail.com>
2023-07-21 00:36:41 +0530

    Temperature will be in Celcius ( &units=metric )

--- src/component/Card.js
+++ src/component/Card.js
@@ -7,7 +7,7 @@ const Card = () => {
 
     useEffect(() => {
         const fetchAPI=async ()=>{
-            const url=`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=29c21bace495743624274095c8a2fd65`;
+            const url=`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=29c21bace495743624274095c8a2fd65`;
             const response=await fetch(url);
             const data=await response.json();
             console.log(data);
@@ -16,8 +16,6 @@ const Card = () => {
 
         fetchAPI();
     },[search] );
-
-    // (32°F − 32) × 5/9 = 0°C
   return (
     <div className='container'>
             <div>
@@ -30,10 +28,8 @@ const Card = () => {
                 ):(<div className='text'>
                 <h2 className='text'>{search}</h2>
                 {/* <h2>Country : {city.sys.country}</h2> */}
-                <h4 className='cur_temp'>Current Temperature : { ((city.temp-32)*(5/9)).toFixed(2)
-
-                    } <sup>o</sup>C</h4>
-                <p className='text'>min temp {((city.temp_min-32)*(5/9)).toFixed(2)}<sup>o</sup>C | max temp : {((city.temp_max-32)*(5/9)).toFixed(2)}<sup>o</sup>C</p>
+                <h4 className='cur_temp'>Current Temperature : {city.temp} <sup>o</sup>C</h4>
+                <p className='text'>min temp {city.temp_min}<sup>o</sup>C | max temp : {city.temp_max}<sup>o</sup>C</p>
                 <p className='text'>Humidity : {city.humidity}</p>
                 <p className='text'>Pressure : {city.pressure}</p>
             </div>)
